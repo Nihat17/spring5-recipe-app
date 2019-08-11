0d9257e01daadebbbaf738da3b2c68980e970078
Nihat <nihatallahverdiyev@gmail.com>
2019-08-11 18:04:45 +0200

    Adding Query method examples on repositories

--- src/main/java/guru/springframework/recipeapp/controllers/IndexControllers.java
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexControllers.java
@@ -1,14 +1,34 @@
 package guru.springframework.recipeapp.controllers;
 
+import guru.springframework.recipeapp.domain.Category;
+import guru.springframework.recipeapp.domain.UnitOfMeasure;
+import guru.springframework.recipeapp.repositories.CategoryRepository;
+import guru.springframework.recipeapp.repositories.UnitOfMeasureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexControllers {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Autowired
+    public IndexControllers(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","index"})
     public String getIndexPage(){
-        System.out.println("Some string to print fhhhf");
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category id: " + categoryOptional.get().getId());
+        System.out.println("Uom id: " + uomOptional.get().getId());
         return "index";
     }
 }
