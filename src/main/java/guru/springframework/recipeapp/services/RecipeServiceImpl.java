cd086a72640f5c75ea0c81876e0fe61a4ebe34ab
Nihat <nihatallahverdiyev@gmail.com>
2019-08-28 16:53:34 +0200

    Checking circleci

--- src/main/java/guru/springframework/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/recipeapp/services/RecipeServiceImpl.java
@@ -1,6 +1,5 @@
 package guru.springframework.recipeapp.services;
 
-import guru.springframework.recipeapp.bootstrap.RecipeBootstrap;
 import guru.springframework.recipeapp.domain.Recipe;
 import guru.springframework.recipeapp.repositories.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
