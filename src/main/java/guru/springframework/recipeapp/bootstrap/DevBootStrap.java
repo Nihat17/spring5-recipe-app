289f8d403109fe696fdbb8b8e2556dc557c75507
Nihat <nihatallahverdiyev@gmail.com>
2019-08-28 16:46:16 +0200

    checking circleci

--- src/main/java/guru/springframework/recipeapp/bootstrap/DevBootStrap.java
+++ src/main/java/guru/springframework/recipeapp/bootstrap/DevBootStrap.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
